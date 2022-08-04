00d0b9788c7cf3bc28afb5ff08bcb15f097d6135
Javo <javier.cervera@coverwallet.com>
2022-08-04 10:07:02 +0200

    creation and counting of contracts per user

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,8 +1,17 @@
 class Contract < ApplicationRecord
-    belongs_to :user, required: true
+    belongs_to :owner, class_name: "User", foreign_key: "owner", required: true
+    belongs_to :beneficiary, class_name: "User", foreign_key: "beneficiary", required: true
+    
 
     validates :title, presence: true, length: {minimum: 3}
-    validates :user_id, presence: true
     validates :kind, presence: true
-    enum :status, {expired: 0, drafted: 1, approved: 2}
+    validates :payable, inclusion: [true, false]
+
+    enum :status, { expired: 0, 
+                    drafted: 1, 
+                    approved: 2, 
+                    proposed: 3, 
+                    modification_requested: 4,
+                    payment_in_progress: 5,
+                    paid: 6}
 end
