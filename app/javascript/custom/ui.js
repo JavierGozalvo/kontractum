34e9633b4386baeabd2938e2d65ee155eed0bcfc
Javo <javier.cervera@coverwallet.com>
2022-09-28 13:44:44 +0200

    initial blockchain integration

---
+++ app/javascript/custom/ui.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   App.init();
+// });
+// const contractForm = document.querySelector("#contractForm");
+// console.log("Dom charged.");
+
+// contractForm.addEventListener("#submit", (e) => {
+//   const title = contractForm["#title"].value;
+//   App.createContract(title);
+//   console.log(contractForm["#title"].value);
+// });
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Kontractum = artifacts.require("Kontractum");
+
+module.exports = function (deployer) {
+  deployer.deploy(Kontractum);
+};
