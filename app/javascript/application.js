93f6647c2333da06a2a8b98ac5eeaa17a03bf860
Javo <javier.cervera@coverwallet.com>
2022-11-14 13:49:55 +0100

    insert require libraries on application.js file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "custom/dist/bundle";
 
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+//= require truffle-contract
+//= require web3
