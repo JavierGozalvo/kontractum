92159f6f95f98bb287df78f82cd3504cfc74f7be
Javo <javier.cervera@coverwallet.com>
2022-11-14 14:07:49 +0100

    javascript libraries imported to rails

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
+
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
@@ -6,3 +7,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/custom", under: "custom"
+pin "truffle-contract", to: "https://cdn.jsdelivr.net/npm/truffle-contract@4.0.31/index.min.js"
+pin "web3", to: "https://cdn.jsdelivr.net/gh/ethereum/web3.js@1.0.0-beta.36/dist/web3.min.js"
