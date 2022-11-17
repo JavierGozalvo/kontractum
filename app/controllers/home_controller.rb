cd86af9e4a37c1a2d5d770f6f24b6c1a82f7a6cc
Javo <javier.cervera@coverwallet.com>
2022-11-17 19:43:54 +0100

    update home controller for index view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,10 @@ class HomeController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-    @q = Contract.ransack(params[:q])
+    @q = Contract.where(owner: current_user).or(Contract.where(beneficiary: current_user)).ransack(params[:q])
     @contracts = @q.result(distinct: true)
-    @contracts = @contracts.page(params[:page]).order(created_at: :desc).per(22)
-    # @contracts = Contract.all
-    # @contracts = @contracts.by_title(params[:title]) if params[:title].present?
-    # @contracts = @contracts.by_payable(params[:payable]) if params[:payable].present?
+    @contracts = @contracts.page(params[:page]).order(created_at: :desc).per(5)
+    
     @contracts = @contracts.by_owner(params[:q][:owner]) if params.dig(:q, :owner).present?
     @contracts = @contracts.by_beneficiary(params[:q][:beneficiary]) if params.dig(:q, :beneficiary).present?
     @contracts = @contracts.by_status(params[:q][:status]) if params.dig(:q, :status).present?
