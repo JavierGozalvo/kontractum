21e4c40e72b1fa9ed4871c53d1b3e1112b509b41
Javo <javier.cervera@coverwallet.com>
2022-11-17 19:56:51 +0100

    new contract attribute for handle tokenization control

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_160749) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_123626) do
   create_table "contracts", force: :cascade do |t|
     t.string "title"
     t.text "desc"
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_160749) do
     t.integer "beneficiary"
     t.boolean "payable", default: false, null: false
     t.string "contract_file"
+    t.string "firm1"
+    t.string "firm2"
   end
 
   create_table "users", force: :cascade do |t|
