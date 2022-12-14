e2dd18bb15cbf7fda78c5098d5eea6b83786f52b
Javo <javier.cervera@coverwallet.com>
2022-12-14 10:46:24 +0100

    cerrar proyecto

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -182,7 +182,9 @@ class ContractsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_contract
-      @contract = Contract.find(params[:id])
+
+        @contract = Contract.find(params[:id])
+
     end
 
     # Only allow a list of trusted parameters through.
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 	def index
     @q = Contract.where(owner: current_user).or(Contract.where(beneficiary: current_user)).ransack(params[:q])
     @contracts = @q.result(distinct: true)
-    @contracts = @contracts.page(params[:page]).order(created_at: :desc).per(5)
+    @contracts = @contracts.page(params[:page]).order(created_at: :desc).per(6)
     
     @contracts = @contracts.by_owner(params[:q][:owner]) if params.dig(:q, :owner).present?
     @contracts = @contracts.by_beneficiary(params[:q][:beneficiary]) if params.dig(:q, :beneficiary).present?
